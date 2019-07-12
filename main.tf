42761e465a52cf769677b02c3fd7c7deec8a743c
Cloud User <centos@ip-172-31-95-15.ec2.internal>
2019-07-12 23:20:02 +0000

    Initial commit

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ resource "aws_instance" "ansible" {
 	ami = "ami-02eac2c0129f6376b"
 	instance_type = "t2.micro"
 
+	user_data = <<-EOF
+							#!/bin/bash
+							echo "Hello world" > index.html
+							nohup busybox httpd -f -p 8080 &
+							EOF 
+
 	tags {
 		Name = "ansible-test"
 	}
